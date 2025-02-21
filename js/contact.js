e57b198b99ca2ca154fd57fd3c35a8dcb7fd9770
Ansh <divyanshuomar856@gmail.com>
2025-02-21 09:55:02 +0530

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,16 @@
-const menuIcon = document.getElementById('menu-icon');
-const nav = document.getElementById('nav');
+document.querySelector('.messageForm').addEventListener('submit', function(event) {
+    let name = document.querySelector('input[name="name"]').value;
+    let email = document.querySelector('input[name="email"]').value;
+    let subject = document.querySelector('input[name="subject"]').value;
+    let message = document.querySelector('textarea[name="message"]').value;
 
-menuIcon.addEventListener('click', () => {
+    if (!name || !email || !subject || !message) {
+        alert('Please fill out all fields');
+        event.preventDefault();
+    }
+});
+
+document.getElementById('menu-icon').addEventListener('click', function() {
+    let nav = document.getElementById('nav');
     nav.classList.toggle('active');
 });
